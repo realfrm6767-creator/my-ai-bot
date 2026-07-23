5162f338220c2ce552a827521fa68cdbfe275fb2
realfrm6767-creator <realfrm.6767@gmail.com>
2026-07-23 14:23:46 +0330

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,13 @@
+"""
+settings.py
+-----------
+مسئول خواندن و نوشتن فایل settings.json (تنظیمات پویای پروژه).
+
+مقادیر ذخیره‌شده: admins, provider, model, language, memory, temperature
+
+توابعی که در مرحله هفتم پیاده‌سازی می‌شوند:
+    - load_settings()
+    - save_settings(data)
+    - get_setting(key)
+    - update_setting(key, value)
+"""
