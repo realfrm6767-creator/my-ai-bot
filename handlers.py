8196f215796d56618899da7b2c34245624231236
realfrm6767-creator <realfrm.6767@gmail.com>
2026-07-23 14:23:07 +0330

    Create handlers.py

---
+++ handlers.py
@@ -0,0 +1,12 @@
+"""
+handlers.py
+-----------
+تمام Handlerهای تلگرام (دستورات، دکمه‌ها، پیام‌های چت) فقط اینجا نوشته می‌شوند.
+
+توابعی که در مراحل بعدی تکمیل خواهند شد:
+    - panel()
+    - button_handler()
+    - chat_handler()
+    - addadmin_handler()
+    - removeadmin_handler()
+"""
